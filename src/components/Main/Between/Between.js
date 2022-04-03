0bd5aa3f2aa4914b8e208cb7fcdf46138990093f
ii4elka <konovalenko.it@gmail.com>
2022-04-03 19:41:27 +0300

    styled components added to Between component

--- src/components/Main/Between/Between.js
+++ src/components/Main/Between/Between.js
@@ -1,13 +1,42 @@
 import s from './Between.module.css'
 import beans from "../../../images/beansBlack.svg";
+import styled from "styled-components";
+
+const Wrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-top: ${(props) => props.margin}px;
+`
+
+const ImgWrapper = styled.div`
+  position: relative;
+  ::before, ::after{
+    content: '';
+    width: 60px;
+    height: 1px;
+    background-color: #000;
+    position: absolute;
+    top: 50%;
+    transform: translateY(-50%);
+  }
+  ::before{
+    left: -85px;
+  }
+  ::after{
+    right: -85px;
+  }
+  
+`
 
 const Between = ({margin}) => {
+    console.log(margin)
     return(
-        <div className={s.between} style={{marginTop: Number(margin)}}>
-            <div className={s.imgWrapper}>
-                <img src={beans} alt="beans" className={s.beans}/>
-            </div>
-        </div>
+        <Wrapper margin={margin}>
+            <ImgWrapper>
+                <img src={beans} alt="beans"/>
+            </ImgWrapper>
+        </Wrapper>
     )
 }
 
