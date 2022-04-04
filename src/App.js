14151b4bdbb59627c07a262a96c2af58ef60d327
ii4elka <konovalenko.it@gmail.com>
2022-04-04 19:03:02 +0300

    Filtter and Search were realised

--- src/App.js
+++ src/App.js
@@ -7,8 +7,8 @@ function App() {
     return (
         <div className="App">
             {/*<Main/>
-            <OurCoffee/>*/}
-            <Pleasure/>
+            <Pleasure/>*/}
+            <OurCoffee/>
 
         </div>
     );
--- src/components/OurCoffee/Filter/Filter.js
+++ src/components/OurCoffee/Filter/Filter.js
@@ -1,30 +1,36 @@
 import styled from 'styled-components'
 import coffee from '../../../images/thirdCoffee.jpg'
+import {Component} from "react";
 
 const FilterTag = styled.section`
   padding: 60px 0 30px;
-  .container{
+
+  .container {
     max-width: 1180px;
     padding: 0 20px;
     margin: 0 auto;
   }
-  .filters{
+
+  .filters {
     max-width: 700px;
     display: flex;
     margin: 0 auto;
     justify-content: space-between;
     align-items: center;
   }
-  .lookFor{
+
+  .lookFor {
     display: flex;
     align-items: center;
-    .lookText{
+
+    .lookText {
       margin-right: 19px;
       font-size: 14px;
       line-height: 20px;
       color: #000000;
     }
-    .input{
+
+    .input {
       background: #FFFFFF;
       box-shadow: 4px 4px 20px rgba(0, 0, 0, 0.25);
       border-radius: 4px;
@@ -34,7 +40,8 @@ const FilterTag = styled.section`
       border: none;
       padding: 0 10px;
     }
-    .input::placeholder{
+
+    .input::placeholder {
       font-size: 12px;
       line-height: 17px;
       text-align: center;
@@ -42,16 +49,19 @@ const FilterTag = styled.section`
 
     }
   }
-  .filter{
+
+  .filter {
     display: flex;
     align-items: center;
-    .filterText{
+
+    .filterText {
       margin-right: 35px;
       font-size: 14px;
       line-height: 20px;
       color: #000000;
     }
-    button{
+
+    button {
       width: 75px;
       height: 30px;
       font-weight: 400;
@@ -65,12 +75,19 @@ const FilterTag = styled.section`
       border-radius: 4px 0px 0px 4px;
       margin-right: 5px;
       cursor: pointer;
-      :last-child{
+      transition: background-color .3s;
+
+      :last-child {
         margin: 0;
       }
     }
+
+    .active {
+      background-color: #eeb400;
+    }
   }
-  .gridWrapper{
+
+  .gridWrapper {
     display: grid;
     justify-content: center;
     max-width: 900px;
@@ -80,32 +97,37 @@ const FilterTag = styled.section`
     grid-template-rows: minmax(260px, auto);
     grid-auto-rows: minmax(260px, auto);
     grid-row-gap: 60px;
-    .item{
+
+    .item {
       filter: drop-shadow(5px 5px 15px rgba(0, 0, 0, 0.25));
       padding: 37px 27px 22px 26px;
       background: #FFFFFF;
       border-radius: 8px;
       cursor: pointer;
       transition: filter .3s;
-      :hover{
+
+      :hover {
         filter: drop-shadow(5px 5px 35px rgba(0, 0, 0, 0.25));
       }
     }
-    .name{
+
+    .name {
       font-size: 14px;
       line-height: 20px;
       text-align: right;
       color: #000000;
       margin-top: 14px;
     }
-    .country{
+
+    .country {
       font-size: 14px;
       line-height: 20px;
       text-align: right;
       color: #000000;
       margin-top: 14px;
     }
-    .price{
+
+    .price {
       font-size: 14px;
       line-height: 20px;
       text-align: right;
@@ -113,83 +135,129 @@ const FilterTag = styled.section`
       margin-top: 14px;
     }
   }
+
+
 `
 
-const Filter = () => {
-    const data = [
-        {
-            name: 'AROMISTICO Coffee 1 kg',
-            country: 'Brazil',
-            price: '6.99$',
-            id: 1
-        },
-        {
-            name: 'AROMISTICO Coffee 1 kg',
-            country: 'Kenya',
-            price: '6.99$',
-            id: 2
-        },
-        {
-            name: 'AROMISTICO Coffee 1 kg',
-            country: 'Columbia',
-            price: '6.99$',
-            id: 3
-        },
-        {
-            name: 'AROMISTICO Coffee 1 kg',
-            country: 'Brazil',
-            price: '6.99$',
-            id: 4
-        },
-        {
-            name: 'AROMISTICO Coffee 1 kg',
-            country: 'Brazil',
-            price: '6.99$',
-            id: 5
-        },
-        {
-            name: 'AROMISTICO Coffee 1 kg',
-            country: 'Brazil',
-            price: '6.99$',
-            id: 6
+class Filter extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            data: [
+                {
+                    name: 'AROMISTICO Coffee 1 kg',
+                    country: 'Brazil',
+                    price: '6.99$',
+                    id: 1
+                },
+                {
+                    name: 'Americano Coffee 1 kg',
+                    country: 'Kenya',
+                    price: '6.99$',
+                    id: 2
+                },
+                {
+                    name: 'Espresso Coffee 1 kg',
+                    country: 'Columbia',
+                    price: '6.99$',
+                    id: 3
+                },
+                {
+                    name: 'Irish Coffee 1 kg',
+                    country: 'Brazil',
+                    price: '6.99$',
+                    id: 4
+                },
+                {
+                    name: 'AROMISTICO Coffee 1 kg',
+                    country: 'Brazil',
+                    price: '6.99$',
+                    id: 5
+                },
+                {
+                    name: 'Lungo Coffee 1 kg',
+                    country: 'Brazil',
+                    price: '6.99$',
+                    id: 6
+                }
+            ],
+            value: '',
+            activeBtn: ''
         }
-    ]
+    }
 
+    searchData = (value, data) => {
+        if (value === ''){
+            return data
+        }
+        return data.filter(obj => (obj.name.toLowerCase().indexOf(value.toLowerCase()) > -1))
+    }
 
-    return (
-        <FilterTag>
-            <div className="container">
-                <div className="filters">
-                    <div className="lookFor">
-                        <div className="lookText">
-                            Looking for
-                        </div>
-                        <input type="text" className='input' placeholder='start typing here...'/>
-                    </div>
-                    <div className="filter">
-                        <div className="filterText">
-                            Or filter
-                        </div>
-                        <button>Brazil</button>
-                        <button>Kenya</button>
-                        <button>Columbia</button>
-                    </div>
-                </div>
-                <div className="gridWrapper">
-                    {data.map(obj => {
-                        return (
-                            <div className='item' key={obj.id}>
-                                <img src={coffee} alt="coffee"/>
-                                <div className="name">{obj.name}</div>
-                                <div className="country">{obj.country}</div>
-                                <div className="price">{obj.price}</div>
+    filterData = (value, data) => {
+        switch (value){
+            case 'Brazil': return data.filter(obj => obj.country === value);
+            case 'Columbia': return data.filter(obj => obj.country === value);
+            case 'Kenya': return data.filter(obj => obj.country === value);
+            default: return data;
+        }
+
+    }
+
+    changeValue = (e) => {
+        this.setState({value: e.target.value})
+    }
+
+    changeButton = (e) => {
+        console.log(e.target.innerHTML, this.state.activeBtn)
+        if (e.target.innerHTML === this.state.activeBtn){
+            this.setState({activeBtn: ''})
+            return;
+        }
+        this.setState({activeBtn: e.target.innerHTML})
+    }
+
+    render() {
+        const {value, data, activeBtn} = this.state
+        let visibleData = this.searchData(value, data)
+        let lastData = this.filterData(activeBtn, visibleData)
+
+        return (
+            <FilterTag>
+                <div className="container">
+                    <div className="filters">
+                        <div className="lookFor">
+                            <div className="lookText">
+                                Looking for
                             </div>
-                        )
-                    })}
+                            <input type="text" className='input' placeholder='start typing here...' onChange={(e) => this.changeValue(e)}/>
+                        </div>
+                        <div className="filter">
+                            <div className="filterText">
+                                Or filter
+                            </div>
+                            <button className={activeBtn === 'Brazil' ? 'active' : ''} onClick={(e) => this.changeButton(e)}>Brazil</button>
+                            <button className={activeBtn === 'Kenya' ? 'active' : ''} onClick={(e) => this.changeButton(e)}>Kenya</button>
+                            <button className={activeBtn === 'Columbia' ? 'active' : ''} onClick={(e) => this.changeButton(e)}>Columbia</button>
+                        </div>
+                    </div>
+                    <div className="gridWrapper">
+                        {lastData.map(obj => {
+                            return (
+                                <div className='item' key={obj.id}>
+                                    <img src={coffee} alt="coffee"/>
+                                    <div className="name">{obj.name}</div>
+                                    <div className="country">{obj.country}</div>
+                                    <div className="price">{obj.price}</div>
+                                </div>
+                            )
+                        })}
+                    </div>
                 </div>
-            </div>
-        </FilterTag>
-    )
+            </FilterTag>
+        )
+    }
+
+
 }
 
 export default Filter;
